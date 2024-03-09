e9e2804ea66c2e1b4aa683b637d4dc12476f3edc
Ravikant tiwari <115395652+theravikanttiwari@users.noreply.github.com>
2024-03-09 23:26:56 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,8 @@
+//practice qs2
+let gamenumber = 25;
+let username = prompt("guess the game number :");
+//console.log(username);
+while(username != gamenumber){
+    usernum = prompt("you entered wrong number.guess again:");
+}
+console.log("congratulations,you entered the right number");
